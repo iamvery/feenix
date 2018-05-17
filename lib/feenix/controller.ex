a86e4ace35e54ab08832a86357039e8c7676456b
Jay Hayes <ur@iamvery.com>
2018-05-17 10:29:34 -0500

    7. Generate controller framework logic with macro
    Similar to the endpoint, the implementation details for the controller
    pipeline can be generated with a macro. But be warned, there's a
    problem! Plugs are run in the order they're defined and this
    implementation runs `apply_action` before anything else... See the next
    commit.

--- lib/feenix/controller.ex
+++ lib/feenix/controller.ex
@@ -1,3 +1,18 @@
 defmodule Feenix.Controller do
-  #
+  defmacro __using__(_opts) do
+    quote do
+      use Plug.Builder
+      plug(:apply_action)
+
+      def call(conn, action) do
+        conn
+        |> put_private(:action, action)
+        |> super(nil)
+      end
+
+      def apply_action(conn, _opts) do
+        apply(__MODULE__, conn.private.action, [conn])
+      end
+    end
+  end
 end
--- lib/your_app/controller.ex
+++ lib/your_app/controller.ex
@@ -1,16 +1,5 @@
 defmodule YourApp.Controller do
-  use Plug.Builder
-  plug(:apply_action)
-
-  def call(conn, action) do
-    conn
-    |> put_private(:action, action)
-    |> super(nil)
-  end
-
-  def apply_action(conn, _opts) do
-    apply(__MODULE__, conn.private.action, [conn])
-  end
+  use Feenix.Controller
 
   def index(conn) do
     send_resp(conn, 200, "meows")
