c3dc0e4225c5c44c61ef6b23f1e9e77a8a17cf1c
Jay Hayes <ur@iamvery.com>
2018-05-17 08:42:03 -0500

    1. Turn endpoint into a pipeline
    The Endpoint is the entry point for a request in the Phoenix pipeline.
    You'll start to see this theme: everything in Phoenix is a pipeline. The
    endpoint, the router, the controller. The endpoint is pluggable so that
    other transformations may be added ahead of routing requests. Think of
    this much like Rack middelware.

--- lib/your_app/endpoint.ex
+++ lib/your_app/endpoint.ex
@@ -1,3 +1,16 @@
 defmodule YourApp.Endpoint do
-  #
+  use Plug.Builder
+
+  plug(:hello)
+  plug(:world)
+
+  def hello(conn, _opts) do
+    IO.puts("hello")
+    conn
+  end
+
+  def world(conn, _opts) do
+    IO.puts("world")
+    conn
+  end
 end
