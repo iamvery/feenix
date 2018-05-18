e3f0c78adc0cf325cf3252d07fc50d22a43f3969
Jay Hayes <ur@iamvery.com>
2018-05-18 11:27:54 -0500

    Deduplicate router DSL with private function

--- lib/feenix/router/dsl.ex
+++ lib/feenix/router/dsl.ex
@@ -1,19 +1,17 @@
 defmodule Feenix.Router.DSL do
   defmacro get(path, module, action) do
-    {_vars, path_info} = Plug.Router.Utils.build_path_match(path)
-
-    quote do
-      def do_match(conn, "GET", unquote(path_info)) do
-        unquote(module).call(conn, unquote(action))
-      end
-    end
+    build("GET", path, module, action)
   end
 
   defmacro post(path, module, action) do
+    build("POST", path, module, action)
+  end
+
+  defp build(method, path, module, action) do
     {_vars, path_info} = Plug.Router.Utils.build_path_match(path)
 
     quote do
-      def do_match(conn, "POST", unquote(path_info)) do
+      def do_match(conn, unquote(method), unquote(path_info)) do
         unquote(module).call(conn, unquote(action))
       end
     end
