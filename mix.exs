95d510350b561a51e887a2438d3db53a5cfe1361
Jay Hayes <ur@iamvery.com>
2018-06-28 09:09:50 -0500

    Use correct app name in mix

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule BuildFeenix.MixProject do
 
   def project do
     [
-      app: :build_feenix,
+      app: :your_app,
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
