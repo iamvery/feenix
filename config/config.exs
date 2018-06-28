c3e57191be880375530ce12220d5bde41ca1b6ef
Jay Hayes <ur@iamvery.com>
2018-06-28 09:10:37 -0500

    Added commented configuration for quick switch to Phoenix

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,10 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+# config :your_app, YourApp.Endpoint,
+#   http: [port: 4000],
+#   server: true
+
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
